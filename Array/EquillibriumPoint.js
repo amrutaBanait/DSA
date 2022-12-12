aa1bc2ec3dcd0c7703aea474d66809cdd5a7da66
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-12 17:08:25 +0530

    find sum

---
+++ Array/EquillibriumPoint.js
@@ -0,0 +1,20 @@
+//To find the equillibrium point in a array
+
+var array = [2,5,7,-3,6,-9];
+var sum = 0;
+var rs = 0;
+var ls=0;
+
+for(let i=0; i<array.length; i++){
+    sum =sum + array[i]
+}
+console.log(sum)
+
+
+
+
+
+
+
+
+
