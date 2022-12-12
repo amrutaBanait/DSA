45a3088357223159ea456ba6b0db1ab94ca66bbf
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-12 17:30:10 +0530

    Unique element

--- Array/UniqueEle.js
+++ Array/UniqueEle.js
@@ -1,5 +1,17 @@
 // To print unique element in the array
 
-var array = [3,6,4,3,7,5,4,5]
+var arr = [3,6,4,3,5,4,5]
 
-
+var obj={};
+for(var i=0;i<arr.length;i++){
+  if(obj[arr[i]]===undefined){
+    obj[arr[i]]=1;
+  }else{
+    obj[arr[i]]=obj[arr[i]]+1;
+  }
+}
+for (key in obj){
+  if(obj[key]==1){
+    console.log((key))
+  }
+}
