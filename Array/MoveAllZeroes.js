c6e73dddb8f5e5514a32ed7244a1c61370598041
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-12 23:18:00 +0530

    move all zeroes to last

---
+++ Array/MoveAllZeroes.js
@@ -0,0 +1,16 @@
+//Given an array, move all zeroes to the end of the array
+
+var array = [2,0,0,4,0,9,0,5,0,5]
+var zeroes = [];
+var others = [];
+var result = [];
+
+for(let i=0; i<array.length; i++){
+    if(array[i] == 0){
+       zeroes.push(array[i])
+    }else{
+        others.push(array[i])
+    }
+}
+result.push(others,zeroes)
+console.log(result.concat())
