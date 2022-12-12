092edda5e9624092ed249c124417d8a3b333d015
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-12 17:13:11 +0530

    found eq point

--- Array/EquillibriumPoint.js
+++ Array/EquillibriumPoint.js
@@ -1,6 +1,6 @@
 //To find the equillibrium point in a array
+var array=[-7,1,5,6,-4,3,0]
 
-var array = [2,5,7,-3,6,-9];
 var sum = 0;
 var rs = 0;
 var ls=0;
@@ -8,13 +8,27 @@ var ls=0;
 for(let i=0; i<array.length; i++){
     sum =sum + array[i]
 }
-console.log(sum)
+//console.log(sum)
+rs = sum;
+
+for(let i=0; i<array.length; i++){
+    rs = rs - array[i];
+    if(ls == rs){
+        console.log(i);
+    }else{
+        ls = ls + array[i]
+    }
+}
 
 
 
+// var arr=[-7,1,5,6,-4,3,0];
 
 
-
-
-
-
+// if(ls==rs){
+//   console.log(i);
+//  // break;
+// }else{
+// ls= ls +arr[i];
+// }
+// }
