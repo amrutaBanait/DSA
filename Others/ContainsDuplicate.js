32b93a908a905a61b6d94a19069e53abcdf5fc11
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-12 22:10:35 +0530

    contains Duplicate

---
+++ Others/ContainsDuplicate.js
@@ -0,0 +1,14 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//Difficulty: Easy
+
+function containsDuplicate(arr) {
+    let hashMap = {};
+    for(let i=0;i<arr.length;i++) {
+        if(hashMap[arr[i]] != undefined) {
+            return true;
+        }
+        hashMap[arr[i]] = 1;
+    }
+    return false;
+}
+console.log(containsDuplicate([2,5,7,4,3,2,4]))
