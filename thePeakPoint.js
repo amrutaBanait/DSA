6b50adf899daba10d2a1214090a248aca68a007a
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-12-22 11:34:03 +0530

    find peak point

---
+++ thePeakPoint.js
@@ -0,0 +1,27 @@
+function peakPoint(array) {
+    let sortedArray = [...array];
+    sortedArray.pop;
+    sortedArray.shift();
+    sortedArray.sort((a, b) => b - a);
+
+    const number = sortedArray[0];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === number) return i;
+    }
+}
+
+function runProgram(input) {
+    var newInput = input.trim().split("\n");
+    for (let i = 2; i < newInput.length; i += 2) {
+        let array = newInput[i].trim().split(" ").map(Number);
+
+        console.log(peakPoint(array));
+    }
+}
+
+let input = `2
+3
+10 20 11
+5
+1 3 6 5 4`;
